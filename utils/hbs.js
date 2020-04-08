cd71494a2b2bccc58d782e526e43e7209237da2e
Akshay <akshay28venkat@gmail.com>
2020-04-08 15:44:37 +0530

    Adding the commnets

--- utils/hbs.js
+++
@@ -1,9 +0,0 @@
-const hbs = require('hbs');
-
-hbs.registerHelper("blogUpdate", function(){
-  return `/blog/update/${this.id}?httpmethod=PATCH`
-})
-
-hbs.registerHelper("blogDelete", function(){
-  return `/blog/delete/${this.id}?httpmethod=DELETE`
-})
