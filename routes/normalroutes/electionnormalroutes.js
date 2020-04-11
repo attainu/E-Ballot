7e41fded058ffa9172b599dcfc006665db19bec5
Akshay Venkat <53833249+Akshay-Venkat-aug6@users.noreply.github.com>
2020-04-11 19:16:43 +0530

    Delete electionnormalroutes.js

--- routes/normalroutes/electionnormalroutes.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require('express');
-
-const { resultElection } = require('../../controllers/normalcontrollers/electionnormalcontroller');
-
-const router = Router();
-
-router.get('/election/result/:electionid', resultElection)
-
-module.exports = router;
