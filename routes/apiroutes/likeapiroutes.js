43ce98823ecd0c19d711719c6bb0bce77976442f
Akshay Venkat <53833249+Akshay-Venkat-aug6@users.noreply.github.com>
2020-04-11 19:14:57 +0530

    Delete likeapiroutes.js

--- routes/apiroutes/likeapiroutes.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require('express');
-const { likeBlog } = require('../../controllers/apicontrollers/likeapicontroller');
-const autheticate = require('../../middleware/autheticate');
-
-const router = Router();
-
-router.post('/blog/like/:blogid',autheticate, likeBlog)
-
-module.exports = router
