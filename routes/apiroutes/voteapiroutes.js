87207f45ff7173cb3b88a49c4a18ff9430dda12e
Akshay Venkat <53833249+Akshay-Venkat-aug6@users.noreply.github.com>
2020-04-11 19:15:09 +0530

    Delete voteapiroutes.js

--- routes/apiroutes/voteapiroutes.js
+++
@@ -1,9 +0,0 @@
-const {  Router } = require('express');
-const { votetoNominee } = require('../../controllers/apicontrollers/voteapicontroller');
-const autheticate = require('../../middleware/autheticate');
-
-const router = Router();
-
-router.post('/vote/:electionid/:nomineeid', autheticate, votetoNominee);
-
-module.exports = router;
